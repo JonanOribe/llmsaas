edd938549840ca9aeb2a83c6e490219e9d47abf9
Jon Ander Oribe <jonan.oribe@gmail.com>
2025-06-18 10:33:45 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import CompanionCard from '@/components/CompanionCard'
 import CompanionsList from '@/components/CompanionsList'
-import CTA from '@/components/cta'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
 import { recentSessions } from '@/constants'
 import React from 'react'
